86ccd3bcf263dd960a9c6a0f8eec1b934262aa47
MikazukiHikari <e24082117@gs.ncku.edu.tw>
2022-04-05 20:16:50 +0800

    drag issue solved

--- js/main1.js
+++ js/main1.js
@@ -1,17 +1,23 @@
  
 
- 
- /**
+/**
  * Drag and Drop multiple elements in multiple sources 
 **/
 
 let sourceContainerId = ''
 
 // Allow multiple draggable items
-let dragSources = document.querySelectorAll('[draggable="true"]')
-dragSources.forEach(dragSource => {
-  dragSource.addEventListener('dragstart', dragStart)
-})
+let dragSources = document.querySelector('#home_liver')
+let dragSources1 = document.querySelector('#home_cat')
+let dragSources2 = document.querySelector('#home_freedom')
+let dragSources3 = document.querySelector('#home_cheerful')
+let dragSources4 = document.querySelector('#home_meme')
+  dragSources.addEventListener('dragstart', dragStart)
+  dragSources1.addEventListener('dragstart', dragStart1)
+  dragSources2.addEventListener('dragstart', dragStart2)
+  dragSources3.addEventListener('dragstart', dragStart3)
+  dragSources4.addEventListener('dragstart', dragStart4)
+  
 
 // Allow multiple dropped targets
 let dropTargets = document.querySelectorAll('[data-role="drag-drop-container"]')
@@ -26,25 +32,57 @@ function cancelDefault (e) {
   e.stopPropagation()
   return false
 }
-
 function dragStart (e) {
   e.dataTransfer.setData('text/plain', e.target.id)
   sourceContainerId = this.parentElement.id
   console.log('sourceContainerId', sourceContainerId)
+  $("#home_liver").fadeOut(10);
+}
+function dragStart1 (e) {
+  e.dataTransfer.setData('text/plain', e.target.id)
+  sourceContainerId = this.parentElement.id
+  console.log('sourceContainerId', sourceContainerId)
+  $("#home_cat").fadeOut(10);
+}
+function dragStart2 (e) {
+  e.dataTransfer.setData('text/plain', e.target.id)
+  sourceContainerId = this.parentElement.id
+  console.log('sourceContainerId', sourceContainerId)
+  $("#home_freedom").fadeOut(10);
+}
+function dragStart3 (e) {
+  e.dataTransfer.setData('text/plain', e.target.id)
+  sourceContainerId = this.parentElement.id
+  console.log('sourceContainerId', sourceContainerId)
+  $("#home_cheerful").fadeOut(10);
+}
+function dragStart4 (e) {
+  e.dataTransfer.setData('text/plain', e.target.id)
+  sourceContainerId = this.parentElement.id
+  console.log('sourceContainerId', sourceContainerId)
+  $("#home_meme").fadeOut(10);
 }
 
 function dropped (e) {
   // execute function only when target container is different from source container
   if (this.id !== sourceContainerId) {
     cancelDefault(e)
-    let id = e.dataTransfer.getData('text/plain')
-    e.target.appendChild(document.querySelector('#' + id))
+    /*let id = e.dataTransfer.getData('text/plain')
+    e.target.appendChild(document.querySelector('#' + id))*/
+  alert("source to target!")
+  $("#home_cat").fadeIn(10);
+  $("#home_liver").fadeIn(10);
+  $("#home_cheerful").fadeIn(10);
+  $("#home_freedom").fadeIn(10);
+  $("#home_meme").fadeIn(10);
+  }else{
+  alert("source to source!")
+  $("#home_cat").fadeIn(10);
+  $("#home_liver").fadeIn(10);
+  $("#home_cheerful").fadeIn(10);
+  $("#home_freedom").fadeIn(10);
+  $("#home_meme").fadeIn(10);
   }
-  $("#home_bowl").fadeOut(500);
-  $("#home_bowl").delay(600).fadeIn(700);
-  /* Because I don't know how to convert the section , I setup it to fadeOut and fadeIn. 
-     When you making the effect, just overwritten line 43&44 to you want!!! 
-  */
 }
 // End of Drag and Drop Basic 
   
